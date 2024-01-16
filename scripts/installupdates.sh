e11951de76875b8d5d2a5abbc049c2dc5241ae05
kalidyasin <kalidyasin29@gmail.com>
2024-01-16 20:31:39 +0300

    add: more photos to wallpaper

--- scripts/installupdates.sh
+++ scripts/installupdates.sh
@@ -11,7 +11,7 @@
 # ----------------------------------------------------- 
 
 sleep 1
-clear
+# clear
 
 cat <<"EOF"
  _   _           _       _            
@@ -23,54 +23,55 @@ cat <<"EOF"
 
 EOF
 
-_isInstalledYay() {
-    package="$1";
-    check="$(yay -Qs --color always "${package}" | grep "local" | grep "${package} ")";
-    if [ -n "${check}" ] ; then
-        echo 0; #'0' means 'true' in Bash
-        return; #true
-    fi;
-    echo 1; #'1' means 'false' in Bash
-    return; #false
-}
+# _isInstalledYay() {
+#     package="$1";
+#     check="$(yay -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+#     if [ -n "${check}" ] ; then
+#         echo 0; #'0' means 'true' in Bash
+#         return; #true
+#     fi;
+#     echo 1; #'1' means 'false' in Bash
+#     return; #false
+# }
 
 # ------------------------------------------------------
 # Confirm Start
 # ------------------------------------------------------
 
-if gum confirm "DO YOU WANT TO START THE UPDATE NOW?" ;then
-    echo "Update started."
-elif [ $? -eq 130 ]; then
-        exit 130
-else
-    echo "Update canceled."
-    exit;
-fi
-echo ""
+# if gum confirm "DO YOU WANT TO START THE UPDATE NOW?" ;then
+#     echo "Update started."
+# # elif [ $? -eq 130 ]; then
+# #         exit 130
+# else
+#     echo "Update canceled."
+#     exit;
+# fi
+# echo ""
 
-if [[ $(_isInstalledYay "Timeshift") == 1 ]] ;then
-    if gum confirm "DO YOU WANT TO CREATE A SNAPSHOT?" ;then
-        echo ""
-        c=$(gum input --placeholder "Enter a comment for the snapshot...")
-        sudo timeshift --create --comments "$c"
-        sudo timeshift --list
-        sudo grub-mkconfig -o /boot/grub/grub.cfg
-        echo "DONE. Snapshot $c created!"
-        echo ""
-    elif [ $? -eq 130 ]; then
-        echo "Snapshot canceled."
-        exit 130
-    else
-        echo "Snapshot canceled."
-    fi
-    echo ""
-fi
+# if [[ $(_isInstalledYay "Timeshift") == 1 ]] ;then
+#     if gum confirm "DO YOU WANT TO CREATE A SNAPSHOT?" ;then
+#         echo ""
+#         c=$(gum input --placeholder "Enter a comment for the snapshot...")
+#         sudo timeshift --create --comments "$c"
+#         sudo timeshift --list
+#         sudo grub-mkconfig -o /boot/grub/grub.cfg
+#         echo "DONE. Snapshot $c created!"
+#         echo ""
+#     elif [ $? -eq 130 ]; then
+#         echo "Snapshot canceled."
+#         exit 130
+#     else
+#         echo "Snapshot canceled."
+#     fi
+#     echo ""
+# fi
 
 echo "-----------------------------------------------------"
 echo "Start update"
 echo "-----------------------------------------------------"
 echo ""
 
-yay
+sudo dnf upgrade --refresh -y && flatpak update -y
+# yay
 
 notify-send "Update complete"
